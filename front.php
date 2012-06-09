b5773aab24b13ede798f6875e3eb12a961f660ec
root <root@vps0.(none)>
2012-06-09 06:39:22 +0000

    this templating looks to be working

--- front.php
+++ front.php
@@ -1,22 +1,22 @@
 <?php
 
-// implementation of sparkplatform api
-$page_title = 'SparkPlatform Api Concept';
-
-$header = '<h1>* Spark *</h1>';
-$content = '<div class="content">' . $page_title . '</div>';
-$footer = '<footer>adios</footer>';
-
 include('vendor/mustache/Mustache.php');
 
-$engine = new Mustache;
-$engine=>template_file = 'templates/layout.html.mustache';
+// implementation of sparkplatform api
+$page_title = 'SparkPlatform Api Concept';
+$header = '* Spark *';
+$content = $page_title;
+$footer = 'adios';
 
-echo $engine->render(
+$m = new Mustache;
+$layout = file_get_contents('templates/layout.html.mustache');
+
+echo $m->render(
+	$layout,
 	array(
-		$page_title,
-		$header,
-		$content,
-		$footer,
+	  'page_title' => $page_title,
+	  'header' => $header,
+	  'content' => $content,
+	  'footer' => $footer
 	)
 );
